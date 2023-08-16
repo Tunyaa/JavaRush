db0a8e4fecf565ffa6940d98ad4ba341cf1b5f18
Tunyaa <oldcampthunder@gmail.com>
2023-08-16 11:46:35 +0300

    JavaRush

---
+++ JavaRush/src/main/java/com/java/javarush/JavaRush.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.java.javarush;
+
+/**
+ *
+ * @author sergey
+ */
+public class JavaRush {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
